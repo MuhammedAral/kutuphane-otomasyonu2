fe7c1159a6606382c4cdfc3a6640d4ab63030cdf
Muhammed Aral <mhmmdalarl@gmail.com>
2025-12-17 19:38:53 +0300

    feat: API URL Render.com bulut sunucusuna guncellendi - EXE artik her yerde calisir

--- csharp/ApiService.cs
+++ csharp/ApiService.cs
@@ -13,7 +13,7 @@ namespace KutuphaneOtomasyon
         { 
             PropertyNameCaseInsensitive = true 
         };
-        private static string _baseUrl = "http://localhost:5026";
+        private static string _baseUrl = "https://kutuphane-api.onrender.com";
         private static string? _token;
         
         static ApiService()
